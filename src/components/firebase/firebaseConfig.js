5aed6f53afe40aea3480aa6bdd6f05864952fad2
ALAMIN Dolar <you@example.com>
2021-11-11 15:24:06 +0600

    ok

---
+++ src/components/firebase/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyC_cmcKWFXaif_M-CowkDayykccLKpK5HE",
+  authDomain: "dental-point-20829.firebaseapp.com",
+  projectId: "dental-point-20829",
+  storageBucket: "dental-point-20829.appspot.com",
+  messagingSenderId: "903700736185",
+  appId: "1:903700736185:web:7420cf73733d349cfe9368"
+};
